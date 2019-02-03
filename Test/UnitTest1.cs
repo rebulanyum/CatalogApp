f2871728a8388f7d2448dc9d4d9649a9acaf92ba
Izzet Sapkalioglu Ojalvo <izzetsapkali@hotmail.com>
2019-02-03 14:25:06 +0300

    Created initial Solution structure.

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rebulanyum.CatalogApp.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
